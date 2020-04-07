a9e20c2d4bc020476cf0a0f58df472b78f684e64
Matteo Frana <f2@f2.net>
2020-04-07 17:55:12 +0200

    Updated render without crashing test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Thing } from '../src'
+import InlineEdit from '../src'
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Thing />, div)
+    ReactDOM.render(<InlineEdit value="a" onChange={() => fetch('')} />, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 })
